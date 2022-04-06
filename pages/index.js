b0b4ff8ce928ad1f6dee70423400bfe2300ab7eb
Pete VS <pete_vs88@me.com>
2022-04-06 12:58:55 -0600

    initial setup

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Foil Find</title>
+        <meta name="description" content="Foil find" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      Hello
+    </div>
+  )
+}
