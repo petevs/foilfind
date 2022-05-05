40a02183e10c49a73a02eab43723a48455cc0f84
Pete VS <pete_vs88@me.com>
2022-05-05 09:13:55 -0600

    added modal for full description

---
+++ components/product/FullDescription.js
@@ -0,0 +1,12 @@
+import { Modal } from "@mantine/core"
+
+const FullDescription = () => {
+
+    return (
+        <Modal>
+            Hey
+        </Modal>
+    )
+}
+
+export default FullDescription
--- components/product/ShortDescription.js
+++ components/product/ShortDescription.js
@@ -1,7 +1,11 @@
 import parse from 'html-react-parser'
-import { Box, Button, Divider, Spoiler, Text, UnstyledButton } from '@mantine/core'
+import { Box, Button, Divider, Modal, Spoiler, Text, UnstyledButton } from '@mantine/core'
+import { useState } from 'react'
 
 const ShortDescription = ({ description }) => {
+
+
+    const [isOpen, setIsOpen] = useState(true)
 
     const style = {
         height: '175px',
@@ -9,17 +13,33 @@ const ShortDescription = ({ description }) => {
         maskImage: 'linear-gradient(to bottom, black 50%, transparent 100%)'
     }
 
+
+    const openModal = () => setIsOpen(true)
+
     return (
-        <Box>
-            <Divider mt='lg' />
-            <Text weight={900} size='xl' mt='md'>Description</Text>
-            <Box sx={style}>
+        <>
+            <Modal
+                opened={isOpen}
+                onClose={() => setIsOpen(false)}
+                centered
+                overflow='inside'
+                size='xl'
+                p='lg'
+                transition='slide-up'
+            >
                 {parse(description)}
+            </Modal>
+            <Box>
+                <Divider mt='lg' />
+                <Text weight={900} size='xl' mt='md'>Description</Text>
+                <Box sx={style}>
+                    {parse(description)}
+                </Box>
+                <UnstyledButton mt='sm' onClick={openModal}>
+                    <Text underline weight={500}>Show More &gt;</Text>
+                </UnstyledButton>
             </Box>
-            <UnstyledButton mt='sm'>
-                <Text underline weight={500}>Show More &gt;</Text>
-            </UnstyledButton>
-        </Box>
+        </>
     )
 }
 
