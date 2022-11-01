0ee78e1eec597bbaabda511bd76ae41d55277080
Peter Van Straaten <pete@petevs.com>
2022-11-01 09:06:35 -0600

    changed filter and added chevron to table rows

--- components/table/BrandTable.js
+++ components/table/BrandTable.js
@@ -1,5 +1,5 @@
-import { Table, Box, createStyles, Container, Paper, Text, Checkbox, Divider, Button, Title, ActionIcon } from "@mantine/core"
-import { IconAdjustments, IconAdjustmentsHorizontal, IconFilter } from "@tabler/icons"
+import { Table, Box, createStyles, Container, Paper, Text, Checkbox, Divider, Button, Title, ActionIcon, Indicator } from "@mantine/core"
+import { IconAdjustments, IconAdjustmentsHorizontal, IconChevronRight, IconFilter } from "@tabler/icons"
 import Link from "next/link"
 
 
@@ -35,10 +35,17 @@ const BrandTable = ({columns, data}) => {
             <Title order={1}>Brands</Title>
             <Text size='sm' color='dimmed'>A list of all the brands</Text>
           </Box>
-            <Button size='sm' variant='default' leftIcon={<IconAdjustmentsHorizontal size={16} />}>Filters</Button>
+          <Indicator label='3' size={22} color='dark' withBorder>
+            <Button 
+              size='sm'
+              variant='default' 
+              leftIcon={<IconAdjustmentsHorizontal size={16} />}
+              sx={(theme) => ({border: `2px solid ${theme.colors.dark[5]}`})}
+            >Filters</Button>
+          </Indicator>
         </Box>
           <Paper withBorder radius='md'>
-            <Box sx={{display: 'grid', gridTemplateColumns: `repeat(${columns.length}, 1fr)`, padding: '.75rem 1.5rem', backgroundColor: '#F9FAFB', borderRadius: '8px 8px 0 0'}}>
+            <Box sx={{display: 'grid', gridTemplateColumns: `repeat(${columns.length}, 1fr) auto`, padding: '.75rem 1.5rem', backgroundColor: '#F9FAFB', borderRadius: '8px 8px 0 0'}}>
               {
                 columns.map((header, index) => (
                   <Text key={index} size='md' transform='capitalize' weight={600}>
@@ -54,19 +61,24 @@ const BrandTable = ({columns, data}) => {
                     <Box sx={(theme) => ({
                       '& :hover': {
                         cursor: 'pointer',
-                        backgroundColor: theme.colors.blue[0]
-                      }
+                        backgroundColor: theme.colors.blue[0],
+                        '& svg': {
+                          stroke: theme.colors.dark[5]
+                        }
+                      },
                     })}>
                       <Box
                         sx={(theme) => ({
                           display: 'grid', 
-                          gridTemplateColumns: `repeat(${columns.length}, 1fr)`, 
+                          gridTemplateColumns: `repeat(${columns.length}, 1fr) auto`, 
                           padding: '.75rem 1.5rem', 
                           borderBottom: `1px solid ${theme.colors.gray[2]}`,
+                          alignItems: 'center'
                         })}
                       >
                         <Text weight={600} size='md'>{row.brand}</Text>
                         <Text transform='capitalize' color='dimmed'>{checkOfferings(row)}</Text>
+                        <IconChevronRight size={18} color='gray' />
                       </Box>
                     </Box>
                   </Link>
